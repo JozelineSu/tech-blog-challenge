6557bd1ee1a1cee7a9d47c58e04a8528f3410972
unknown <jozelinesuarez@gmail.com>
2024-01-25 16:39:46 -0600

    port change for heroku

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || 3001, () => console.log('Now listening'));
+
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
